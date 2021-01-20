1f233be38b0fd27c825a8f554dae86c6a0081d75
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-01-20 10:32:08 +0000

    Update PvP.php

--- src/NamelessLeaf/PvP.php
+++ src/NamelessLeaf/PvP.php
@@ -1,7 +1,28 @@
 <php?
 
-use pocketmine/player
-use pocketmine/server
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
 
-use pocketmine/Command
-use pocketmine/command/CommandSender
+use pocketmine\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\EventListener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\utils\TextFormat
+      
+class main extends PluginBase implements Listener {
+      
+      public function onEnable(){
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      }
+      
+      public function onCommand(CommandSender $sender, Command $cmd, string $lable, array $args) : bool{
+  
+          switch($cmd->getName()){
+              case "PvP"
+  
+  
+          }
+      }     
+      }
