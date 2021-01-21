3a9815a4af0c9078750354fed6449131bf01c997
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-01-21 09:31:31 +0000

    Update PvP.php

--- src/NamelessLeaf/PvP.php
+++ src/NamelessLeaf/PvP.php
@@ -18,3 +18,47 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\utils\TextFormat;
       
 class pvp extends PluginBase implements Listener {}
+      
+      public function onEnable(){
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      }
+      
+      public function onCommand(CommandSender $sender, Command $cmd, string $lable, array $args) : bool{
+  
+          switch($cmd->getName()){
+                case "pvp":
+                      if($sender instanceof Player){
+                      $this->openMyForm($sender);
+                      }
+  break;
+  
+          }
+  return true;
+      }     
+      
+      
+public function mainForm($player){
+$formapi = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+
+$form = $formapi->createSimpleForm(function (Player $event, array $data){
+$player = $event->getPlayer();
+$result = $data[0];
+if ($result === null) {
+}
+switch ($result) {
+case 1:
+
+    }
+break;
+
+
+
+});
+$form->setTitle("1VS1");
+$form->setContent("PROCEED");
+$form->addButton("MATCH");
+$form->addButton("testc", 1, "https://i.yike.com/yike.jpg");
+$form->sendToPlayer($player);
+
+} 
+      }
