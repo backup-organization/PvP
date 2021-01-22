ded31efcf162f8713d14ae8c596c7a0524f9527e
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-01-22 14:42:17 +0000

    Create Main.php

---
+++ src/NamelessLeaf/Main/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NamelessLeaf\Main;
+
+use pocketmne\Player;
+use pocketmine\Server;
+use pocketmine\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+  
+class main extends PluginBase implements Listener {
+  
+  public function onEnable(){
+    $this->getLogger()->info("enabled");
+  }
+}
