f9fcd485f550d9778070341f13ab556f3b3b6dc8
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-01-22 12:15:33 +0000

    Update Main.php

--- src/NamelessLeaf/Main.php
+++ src/NamelessLeaf/Main.php
@@ -1,5 +1,8 @@
 <?php
+
+namespace NamelessLeaf\Main
+
 use pocketmne/Player;
 use pocketmine/Server;
 use pocketmine/Command
-use pocketmine/command/CommandSender
+use pocketmine/command/CommandSender;
