e4e78a2623862e8a7c82bfd268d7cd8394122e7c
NamelessLeaf <74725613+NamelessLeaf@users.noreply.github.com>
2021-01-22 14:33:57 +0000

    Update Main.php

--- src/NamelessLeaf/Main.php
+++ src/NamelessLeaf/Main.php
@@ -11,5 +11,8 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
   
 class Main extends PluginBase implements Listener {
-
+  
+  public function onEnable(){
+    $this->getLogger()->info("enabled");
+  }
 }
